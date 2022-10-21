02cbd335bd3d3287439ff916175227917dcfab5c
Luana Stephany <ls.luanastephany@gmail.com>
2022-10-21 14:42:27 -0700

    creating header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css";
 
+import logo from "../assets/logo.svg";
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Pentagon Feed</strong>
+      <img src={logo} alt="logo" />
     </header>
   );
 }
