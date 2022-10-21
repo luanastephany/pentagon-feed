7f16d9a15b7f46423d966970a58526981c6eafd7
Luana Stephany <ls.luanastephany@gmail.com>
2022-10-21 16:40:17 -0700

    creating sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { Header } from "./components/Header";
+import styles from "./App.module.css";
+import { Header } from "./components/Header/Header";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 import "./global.css";
+import { Post } from "./Post";
 
 export function App() {
   return (
     <div>
       <Header />
-      <h1>Hello</h1>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Luana" content="A new post" />
+        </main>
+      </div>
     </div>
   );
 }
